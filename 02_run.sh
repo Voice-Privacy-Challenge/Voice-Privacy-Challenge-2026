49c31f60406a9d30d686fef63abdbb18ab30f610
xiaoxiaomiao323 <152830384+xiaoxiaomiao323@users.noreply.github.com>
2026-02-11 23:13:03 +0800

    Uncommented audio anonymization and evaluation scripts

--- 02_run.sh
+++ 02_run.sh
@@ -36,10 +36,10 @@ fi
 
 # Generate anonymized audio (libri dev+test set & IEMOCAP dev+test set & libri-360h)
 echo python run_anonymization.py --config ${anon_config} ${force_compute}
-# python run_anonymization.py --config ${anon_config} ${force_compute}
+python run_anonymization.py --config ${anon_config} ${force_compute}
 
 # Perform libri dev+test & IEMOCAP dev+test pre evaluation using pretrained ASR/ASV/SER models
-# python run_evaluation.py --config $(dirname ${anon_config})/$track1/eval_pre.yaml --overwrite "${eval_overwrite}" ${force_compute}
+python run_evaluation.py --config $(dirname ${anon_config})/$track1/eval_pre.yaml --overwrite "${eval_overwrite}" ${force_compute}
 
 # # Train post ASV using anonymized libri-360 and perform libri dev+test post evaluation
 # # ASV training takes ~2hours
