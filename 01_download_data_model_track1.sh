c432cff4a018869080422e93122e6695bf9246f0
xiaoxiaomiao323 <152830384+xiaoxiaomiao323@users.noreply.github.com>
2026-07-23 21:55:08 +0800

    Update 01_download_data_model_track1.sh

--- 01_download_data_model_track1.sh
+++ 01_download_data_model_track1.sh
@@ -86,7 +86,7 @@ if [ ! -d "exp/$model" ]; then
     cd exp
     if [ ! -f .${model}.zip ]; then
         echo "Download pretrained $model models pre-trained..."
-        wget -O ${model}.zip https://duke.app.box.com/shared/static/na6grb7akap4ze66stiazp2azw4zb1f1
+        wget -O ${model}.zip https://duke.app.box.com/shared/static/wo07rwi5xcgam1v6qlzfnzb536rqug14
         mv ${model}.zip .${model}.zip
     fi
     echo "Unpacking pretrained evaluation models"
